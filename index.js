661f50c52bbae51ac3fffbe378b6c2b738ed21db
ddbr6604smfc <ddbr6604smfc@gmail.com>
2015-10-15 00:12:00 -0500

    working version

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+#!/usr/bin/env node
 
+/**
+ * Module dependencies
+ */
+
+var generate = require('./lib/generate');
+
+var name = process.argv[2];
+
+if (!name) {
+  return console.log('\nComponent name required\n');
+}
+
+generate(name, process.cwd())
+  .then(filename => console.log(`${name} -> ${filename}`))
+  .catch(error => console.log('Oops!', error));
